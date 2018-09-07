b369f3c7dc29f99dbcff555f5ce0b8ba6aec7bcb
svidlonix <sergey.svidlo@nixsolutions.com>
2018-09-07 16:18:20 +0300

    Second Task add question answer controller

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :answer do
+    question
     body 'MyText'
+  end
+
+  factory :invalid_answer, class: 'Answer' do
+    question
+    body nil
+  end
+
+  factory :new_answer, class: 'Answer' do
+    question
+    body 'NewText'
   end
 end
