3bb79658f4f714ce3f9eea80ed8cab156bdd34f7
svidlonix <sergey.svidlo@nixsolutions.com>
2018-10-02 10:15:35 +0300

    Hotfix unicorn config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,5 +1,5 @@
 # paths
-app_path = "/home/deployer/stack_overflow"
+app_path = "/home/deploy/stack_overflow"
 working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
